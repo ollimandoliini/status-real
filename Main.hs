0d5af9d5ce5fd513b0bdb3389ba69250a4b0bf54
Olli Kortelahti <okortelahti@gmail.com>
2020-03-09 10:10:45 +0200

    debugging

--- Main.hs
+++ Main.hs
@@ -17,13 +17,16 @@ main = do
     fileContent <- readFile "data/status.real"
     let pkgs = mapMaybe parsePackageInfo $ T.splitOn "\n\n" $ T.pack fileContent
     scotty port $ do
-        get "/package/:packageName" $ do
-            packageName <- param "packageName"
-            case findPackage packageName pkgs of
-                Just pkg -> html $ packageInfoView pkgs pkg
-                Nothing -> html $ notFoundView packageName
-        get "/" $
-            html $ packageListView pkgs
-        
+      get "/moro" $ html "moro"
+      get "/" $ html "muut"
+
+    -- scotty port $ do
+    --     get "/package/:packageName" $ do
+    --         packageName <- param "packageName"
+    --         case findPackage packageName pkgs of
+    --             Just pkg -> html $ packageInfoView pkgs pkg
+    --             Nothing -> html $ notFoundView packageName
+    --     get "/" $
+    --         html $ packageListView pkgs
     where
         findPackage packageName = find (\x -> name x == packageName)
