9fa0e14984469916b0b4d8997d8739dfde4e1b63
Olli Kortelahti <okortelahti@gmail.com>
2020-03-09 09:50:56 +0200

    changed port

--- Main.hs
+++ Main.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Web.Scotty
-
+import System.Environment (lookupEnv)
 main :: IO ()
-main = scotty 3000 $ do
-  get "/1" $
-    html "1"
-  get "/2" $
-    html "2"
-  get "/" $
-    html "rest"
+main = do 
+  port <- fmap (maybe 3000 read) (lookupEnv "PORT")
+  scotty 3000 $ do
+    get "/1" $
+      html "1"
+    get "/2" $
+      html "2"
+    get "/" $
+      html "rest"
