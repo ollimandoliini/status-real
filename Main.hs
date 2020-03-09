02ca1275e58326f1f34015c13a5fa59a0244e3c3
Olli Kortelahti <okortelahti@gmail.com>
2020-03-09 11:03:16 +0200

    .

--- Main.hs
+++ Main.hs
@@ -8,6 +8,9 @@ import Data.Maybe
 import Data.List (find)
 import System.Environment (lookupEnv)
 import System.Directory (doesFileExist)
+import Network.HTTP.Simple
+import Network.HTTP.Conduit
+import Data.Text.Lazy.Encoding (decodeUtf8)
 
 import Parser (PackageInfo(..), parsePackageInfo)
 import Views
@@ -16,8 +19,8 @@ main :: IO ()
 main = do
     port <- fmap (maybe 3000 read) (lookupEnv "PORT")
     file <- dataFile
-    fileContent <- readFile file
-    let pkgs = mapMaybe parsePackageInfo $ T.splitOn "\n\n" $ T.pack fileContent
+    fileContent <- foo
+    let pkgs = mapMaybe parsePackageInfo $ T.splitOn "\n\n" fileContent
     scotty port $ do
         get "/package/:packageName" $ do
             packageName <- param "packageName"
@@ -34,5 +37,13 @@ main = do
 dataFile :: IO String
 dataFile = do
   fileExists <- doesFileExist "/var/lib/dpkg/status.real"
-  if fileExists then return "/var/lib/dpkg/status.real" else return "./data/status.real"
+  if fileExists then return "/var/lib/dpkg/status.real" else return "/app/data/status.real"
 
+
+-- foo = do
+--   response <- simpleHttp "https://gist.githubusercontent.com/lauripiispanen/29735158335170c27297422a22b48caa/raw/61a0f1150f33a1f31510b8e3a70cbac970892b2f/status.real"
+--   let content = L.toStrict $ decodeUtf8 response
+--   print content
+--   return content
+--   -- let body = fmap rspBody response
+--   -- print body
